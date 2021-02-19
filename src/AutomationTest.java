6859b877df9e0aac4d5934cc04c6f31fbda5052f
Boopathy Johnson <boopathycse7@gmail.com>
2021-02-19 17:06:36 +0530

    File as modified

--- src/AutomationTest.java
+++ src/AutomationTest.java
@@ -3,6 +3,13 @@ public class AutomationTest {
 
 	public static void main(String[] args) {
 System.out.println("English first");
+System.out.println("tamil first");
+
+System.out.println("maths first");
+
+System.out.println("science first");
+System.out.println("social first");
+
 	}
 
 }
